5a212c431d7e39d802a9670e9086d0c9a1f36b17
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-06-11 15:41:12 +0530

    webpack name correction

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, './build/lib'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
     library: '',
     libraryTarget: 'commonjs'
