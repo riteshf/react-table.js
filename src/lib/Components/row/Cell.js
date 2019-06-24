6911e39b0680608fb5cc3e947605544b2f9936ff
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-06-24 14:35:47 +0530

    afterRow check on cell click

--- src/lib/Components/row/Cell.js
+++ src/lib/Components/row/Cell.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Cell = ({ style = {}, value = "", cellIndex = null, setAfterRow, columnOptions, headerOptions }) => {
 
-    const checkAndSetAfterRow = (afterRow) => (afterRow.index === cellIndex) && setAfterRow(afterRow);
+    const checkAndSetAfterRow = (afterRow) => (afterRow && afterRow.index === cellIndex) && setAfterRow(afterRow);
     return (
         <td style={style} onClick={() => checkAndSetAfterRow(headerOptions.afterRow)}>
             {value}
